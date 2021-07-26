a5d9c8c2ac95d123de57ffd27889a91c9f3e214b
ecquinosa <ecquinosa@allcardtech.com.ph>
2021-07-26 20:50:57 +0800

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+namespace SendSmtp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtRecipient = new System.Windows.Forms.TextBox();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(38, 267);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(117, 39);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "SEND";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "HOST";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(244, 30);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(394, 26);
+            this.txtHost.TabIndex = 2;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(244, 75);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(394, 26);
+            this.txtPort.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "PORT";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(244, 120);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(394, 26);
+            this.txtUser.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "EMAIL/ USERID";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(244, 170);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '@';
+            this.txtPassword.Size = new System.Drawing.Size(394, 26);
+            this.txtPassword.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 173);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "PASSWORD";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 222);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(146, 18);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "SEND TO (EMAIL):";
+            // 
+            // txtRecipient
+            // 
+            this.txtRecipient.Location = new System.Drawing.Point(244, 218);
+            this.txtRecipient.Name = "txtRecipient";
+            this.txtRecipient.Size = new System.Drawing.Size(394, 26);
+            this.txtRecipient.TabIndex = 10;
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(38, 328);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.Size = new System.Drawing.Size(600, 131);
+            this.txtLog.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 489);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtRecipient);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtPassword);
+            this.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SMTP TESTER v1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtRecipient;
+        private System.Windows.Forms.TextBox txtLog;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SendSmtp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(credFile))
+            {
+                var decryptedString = AesOperation.DecryptString(key, File.ReadAllText(credFile));
+                txtHost.Text = decryptedString.Split('|')[0];
+                txtPort.Text = decryptedString.Split('|')[1];
+                txtUser.Text = decryptedString.Split('|')[2];
+                txtPassword.Text = decryptedString.Split('|')[3];
+            }
+        }
+
+        private string key = "e171a9998a4e413311112ea2315a1412";
+        private string credFile = Application.StartupPath + "\\cred";
+
+        public bool SendNotification()
+        {
+            SmtpClient client = new SmtpClient();
+
+            try
+            {
+                string emailCred = string.Format("{0}|{1}|{2}|{3}", txtHost.Text, txtPort.Text, txtUser.Text, txtPassword.Text);
+                var encryptedString = AesOperation.EncryptString(key, emailCred);
+                File.WriteAllText(credFile, encryptedString);
+
+                client.Port = Convert.ToInt32(txtPort.Text); //587;
+                client.Host = txtHost.Text; //"smtp.allcard.com.ph";
+                                                                     //client.EnableSsl = true;
+                client.Timeout = 10000; //10000;
+                //client.EnableSsl = true;
+                //client.DeliveryMethod = SmtpDeliveryMethod
+                //client.UseDefaultCredentials = false;
+                //client.Credentials = new System.Net.NetworkCredential(Properties.Settings.Default.SMTP_USER.Split('@')[0], Properties.Settings.Default.SMTP_PASS);//("ecquinosa", "earl1106_1c");
+                client.Credentials = new System.Net.NetworkCredential(txtUser.Text, txtPassword.Text);//("ecquinosa", "earl1106_1c");
+
+
+                MailMessage mm = new MailMessage(txtUser.Text, txtRecipient.Text, "SMTP TEST - " + DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss tt"), "TEST MESSAGE"); //("ecquinosa@allcardtech.com.ph", "ecquinosa@allcardtech.com.ph,vsdelafuente@allcardtech.com.ph,agprotacio@allcardtech.com.ph", "test", "test");
+                //mm.Bcc.Add("ecquinosa@allcardtech.com.ph");
+                mm.BodyEncoding = System.Text.UTF8Encoding.UTF8;
+                mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+
+                //Attachment attachment = new Attachment(fileAttachment, System.Net.Mime.MediaTypeNames.Application.Octet);
+                // Add time stamp information for the file.
+                //ContentDisposition disposition = data.ContentDisposition;
+                //disposition.CreationDate = System.IO.File.GetCreationTime(file);
+                //disposition.ModificationDate = System.IO.File.GetLastWriteTime(file);
+                //disposition.ReadDate = System.IO.File.GetLastAccessTime(file);
+                // Add the file attachment to this email message.
+                //mm.Attachments.Add(attachment);
+
+                client.Send(mm);
+
+                //System.Windows.Forms.MessageBox.Show("Test message sent. Check your inbox");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                txtLog.Text = DateTime.Now.ToString("hh:mm:ss tt ") + "Failed. " + ex.Message;
+                //errMsg = ex.Message;
+                //Utilities.WriteToRTB(string.Format("SendNotification(): Runtime error {0}", errMsg), ref rtb, ref tssl);
+                return false;
+            }
+            finally
+            {
+                client = null;
+                MessageBox.Show("Done!", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            this.Enabled = false;
+            Cursor = Cursors.WaitCursor;
+
+            txtLog.Clear();
+            //System.Threading.Thread.Sleep(2000);
+            Application.DoEvents();            
+
+            if (txtHost.Text == "")
+            {
+                MessageBox.Show("Please complete required fields.", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (txtPort.Text == "")
+            {
+                MessageBox.Show("Please complete required fields.", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (txtUser.Text == "")
+            {
+                MessageBox.Show("Please complete required fields.", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (txtPassword.Text == "")
+            {
+                MessageBox.Show("Please complete required fields.", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (txtRecipient.Text == "")
+            {
+                MessageBox.Show("Please complete required fields.", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (!txtUser.Text.Contains("@"))
+            {
+                MessageBox.Show("Please Email/ UserId if valid.", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (!txtRecipient.Text.Contains("@"))
+            {
+                MessageBox.Show("Please Email/ UserId if valid.", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (SendNotification()) txtLog.Text = DateTime.Now.ToString("hh:mm:ss tt ") + "Success. Check sent email.";
+            
+            this.Enabled = true;
+            Cursor = Cursors.Default;
+        }
+
+        public class AesOperation
+        {
+            public static string EncryptString(string key, string plainText)
+            {
+                byte[] iv = new byte[16];
+                byte[] array;
+
+                using (Aes aes = Aes.Create())
+                {
+                    aes.Key = Encoding.UTF8.GetBytes(key);
+                    aes.IV = iv;
+
+                    ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                    using (MemoryStream memoryStream = new MemoryStream())
+                    {
+                        using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+                        {
+                            using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+                            {
+                                streamWriter.Write(plainText);
+                            }
+
+                            array = memoryStream.ToArray();
+                        }
+                    }
+                }
+
+                return Convert.ToBase64String(array);
+            }
+
+            public static string DecryptString(string key, string cipherText)
+            {
+                byte[] iv = new byte[16];
+                byte[] buffer = Convert.FromBase64String(cipherText);
+
+                using (Aes aes = Aes.Create())
+                {
+                    aes.Key = Encoding.UTF8.GetBytes(key);
+                    aes.IV = iv;
+                    ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                    using (MemoryStream memoryStream = new MemoryStream(buffer))
+                    {
+                        using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+                        {
+                            using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+                            {
+                                return streamReader.ReadToEnd();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
